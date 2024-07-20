8a32600641901523238948c47e5bc8077c625df4
Myroslav <muroslavkl@gmail.com>
2024-07-20 16:42:19 +0300

    feature: add entities

---
+++ src/Domain/Entities/Pet.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Entities.Base;
+
+namespace Domain.Entities;
+
+public class Pet:BaseEntity
+{
+    public string Name { get; set; } = string.Empty;
+    public DateOnly DateOfBirth { get; set; }
+    public DateTime StartDate { get; set; } //start date in shelter
+    public DateTime LastDate { get; set; } //last date in shelter
+    public string TypeOfPet { get; set; } = string.Empty;
+    public string Species { get; set; } = String.Empty;
+    public bool IsVaccination { get; set; } = true;
+    public string Description { get; set; } = string.Empty;
+
+    public ICollection<Image> Images { get; set; } = new List<Image>();
+}
---
+++ src/Domain/Entities/User.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using Domain.Entities.Base;
+
+namespace Domain.Entities;
+
+public class User:BaseEntity
+{
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public DateOnly DateOfBirth { get; set; }
+    public string Gender { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+
+    public int RoleId { get; set; }
+    [ForeignKey("RoleId")]
+    public Role Role { get; set; } = null!;
+
+    public ICollection<Reserve> Reserves { get; set; } = new List<Reserve>();
+}
