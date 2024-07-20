d123ab2b9d8ed5f210993ff9846c55e7eb5f17a4
Myroslav <muroslavkl@gmail.com>
2024-07-20 17:47:40 +0300

    feature: add migration

--- src/API/Program.cs
+++ src/API/Program.cs
@@ -1,10 +1,12 @@
+using Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddInfrastructure(builder.Configuration);
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
---
+++ src/Infrastructure/InfrastructureConfig.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure;
+
+public static class InfrastructureConfig
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<PetsonContext>(options => {
+            options.UseNpgsql(configuration.GetConnectionString("Database"));
+        });
+        return services;
+    }
+}
