728bab53ed98277cfe3ff1111e39e0ad0492d386
Myroslav <muroslavkl@gmail.com>
2024-07-21 15:13:34 +0300

    feature: add DTOs for user

---
+++ src/Application/DTOs/UserDTOs/CreateUserDto.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Application.DTOs.UserDTOs;
+
+public class CreateUserDto
+{
+    [Required,MaxLength(25)]
+    public string FirstName { get; set; } = string.Empty;
+    [Required,MaxLength(25)]
+    public string LastName { get; set; } = string.Empty;
+}
---
+++ src/Application/DTOs/UserDTOs/UserDto.cs
@@ -0,0 +1,12 @@
+﻿namespace Application.DTOs.UserDTOs;
+
+public class UserDto
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public DateOnly DateOfBirth { get; set; }
+    public string Gender { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public int RoleId { get; set; }
+}
