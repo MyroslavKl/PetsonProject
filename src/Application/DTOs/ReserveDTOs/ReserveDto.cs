b3dd9df1c0bd4d340b7f63dac2a38edc332518ce
Myroslav <muroslavkl@gmail.com>
2024-07-21 15:06:02 +0300

    feature: add DTOs for reserve

---
+++ src/Application/DTOs/ReserveDTOs/ReserveDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Application.DTOs.ReserveDTOs;
+
+public class ReserveDto
+{
+    public int Id { get; set; }
+    public DateTime ReserveDate { get; set; }
+    public int PetId { get; set; }
+    public int UserId { get; set; }
+}
---
+++ src/Application/DTOs/ReserveDTOs/UpsertReserveDto.cs
@@ -0,0 +1,6 @@
+﻿namespace Application.DTOs.ReserveDTOs;
+
+public class UpsertReserveDto
+{
+    public DateTime ReserveDate { get; set; } = DateTime.Now;
+}
