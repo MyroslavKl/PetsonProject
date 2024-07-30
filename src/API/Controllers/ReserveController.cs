c000f408ee9ba2d94217d9d8bb121b6bc872ce16
Myroslav <muroslavkl@gmail.com>
2024-07-30 13:23:57 +0300

    feature: implement reserve controller

--- src/API/Controllers/ReserveController.cs
+++ src/API/Controllers/ReserveController.cs
@@ -1,3 +1,6 @@
+using Application.DTOs.ReserveDTOs;
+using Application.Persistence.Repositories;
+using Application.Persistence.Services;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -7,5 +10,44 @@ namespace API.Controllers
     [ApiController]
     public class ReserveController : ControllerBase
     {
+        private readonly IReserveService _reserveService;
+        private readonly IReserveRepository _reserveRepository;
+
+        public ReserveController(IReserveService reserveService,IReserveRepository reserveRepository)
+        {
+            _reserveService = reserveService;
+            _reserveRepository = reserveRepository;
+        }
+
+        [HttpGet("{id}")]
+        public IEnumerable<ReserveDto> GetReserves([FromRoute]int id)
+        {
+            var reserves = _reserveService.GetAllReserves(id);
+            return reserves;
+        }
+
+        [HttpGet("specific-date")]
+        public async Task<IEnumerable<ReserveDto>> GetByDate([FromBody] DateTime date)
+        {
+           var reserves = await _reserveService.GetReservesByDate(date);
+           return reserves;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateReserve([FromBody] UpsertReserveDto reserveDto)
+        {
+            await _reserveService.CreateReserve(reserveDto);
+            return Ok("Reserve is created successfully");
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> RemoveService([FromRoute]int id)
+        {
+            var reserve = await _reserveRepository.GetOneAsync(obj => obj.Id == id);
+            await _reserveService.DeleteReserve(reserve);
+            return Ok("Reserve successfully deleted");
+        }
+
+
     }
 }
