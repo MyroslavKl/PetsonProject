f4df28e02aaf0c36c49a7d9b57fff89576136670
Myroslav <muroslavkl@gmail.com>
2024-08-05 13:52:55 +0300

    feature: create the action filters for Pet controller

---
+++ src/Application/ActionFilters/PetFilters/PetExistByTypeFilterAttribute.cs
@@ -0,0 +1,25 @@
+﻿using System.Net;
+using Application.Persistence.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Application.ActionFilters.PetFilters;
+
+public class PetExistByTypeFilterAttribute(IPetRepository petRepository):ActionFilterAttribute
+{
+    public override async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+    {
+        string type = (string)context.ActionArguments["type"];
+        if (await petRepository.GetOneAsync(obj => obj.TypeOfPet == type) is null)
+        {
+            context.ModelState.AddModelError("Pet", "Pet does not exist.");
+            ValidationProblemDetails details = new ValidationProblemDetails(context.ModelState)
+            {
+                Status = HttpStatusCode.NotFound as int?
+            };
+            context.Result = new NotFoundObjectResult(details);
+            return;
+        }
+        await next();
+    }
+}
