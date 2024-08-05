7eacb3535828662f656d6d2a05c968825c858dc0
Myroslav <muroslavkl@gmail.com>
2024-08-05 14:08:21 +0300

    feature: create action filters for Reserve controller

---
+++ src/Application/ActionFilters/ReserveFilters/ReserveExistByIdFilterAttribute.cs
@@ -0,0 +1,25 @@
+﻿using System.Net;
+using Application.Persistence.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Application.ActionFilters.ReserveFilters;
+
+public class ReserveExistByIdFilterAttribute(IReserveRepository reserveRepository):ActionFilterAttribute
+{
+    public override async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+    {
+        int id = (int)context.ActionArguments["id"];
+        if (await reserveRepository.GetOneAsync(obj => obj.Id == id) is null)
+        {
+            context.ModelState.AddModelError("Reserve", "Reserve does not exist.");
+            ValidationProblemDetails details = new ValidationProblemDetails(context.ModelState)
+            {
+                Status = HttpStatusCode.NotFound as int?
+            };
+            context.Result = new NotFoundObjectResult(details);
+            return;
+        }
+        await next();
+    }
+}
