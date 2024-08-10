0eb0f0fce238aec3a9c27f9c0b6b739383984057
Myroslav <muroslavkl@gmail.com>
2024-08-10 17:29:12 +0300

    feature: add cache service

---
+++ src/CacheServices/CacheConfig.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CacheServices;
+
+public static class CacheConfig
+{
+    public static IServiceCollection AddAuthService(this IServiceCollection services, IConfiguration configuration)
+    {
+
+        return services;
+    }
+}
