39ef829928548c0a8db28d96b809fa07271f859b
Myroslav <muroslavkl@gmail.com>
2024-08-13 16:26:48 +0300

    feature: partly implement caching to image service

--- src/Infrastructure/Persistence/Services/ImageService.cs
+++ src/Infrastructure/Persistence/Services/ImageService.cs
@@ -2,6 +2,7 @@
 using Application.Persistence.Repositories;
 using Application.Persistence.Services;
 using AutoMapper;
+using CacheServices.Service;
 using Domain.Entities;
 
 namespace Infrastructure.Persistence.Services;
@@ -10,14 +11,16 @@ public class ImageService:IImageService
 {
     private readonly IImageRepository _imageRepository;
     private readonly IMapper _mapper;
+    private readonly ICacheService _cacheService;
 
-    public ImageService(IImageRepository imageRepository,IMapper mapper)
+    public ImageService(IImageRepository imageRepository,IMapper mapper,ICacheService cacheService)
     {
         _imageRepository = imageRepository;
         _mapper = mapper;
+        _cacheService = cacheService;
 
     }
-    public IEnumerable<ImageDto> GetAllImages(int petId)
+    public IEnumerable<ImageDto> GetAllImages(int petId) ////
     {
         var images = _imageRepository.GetAll(obj => obj.PetId == petId);
         var imagesDto = _mapper.Map<IEnumerable<ImageDto>>(images);
@@ -29,11 +32,15 @@ public class ImageService:IImageService
         var image = _mapper.Map<Image>(imageDto);
         await _imageRepository.InsertAsync(image);
         await _imageRepository.SaveChangesAsync();
+        var dto = _mapper.Map<ImageDto>(image);
+        var expirationTime = DateTime.Now.AddMinutes(3);
+        _cacheService.SetData($"image{image.Id}",dto,expirationTime);
     }
 
     public async Task DeleteImageAsync(Image image)
     {
         _imageRepository.Delete(image);
         await _imageRepository.SaveChangesAsync();
+        _cacheService.RemoveData($"image{image.Id}");
     }
 }
