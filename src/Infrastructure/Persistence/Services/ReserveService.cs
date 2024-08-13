e3df226962213e6d3a33ebe747bace4c1008d309
Myroslav <muroslavkl@gmail.com>
2024-08-13 17:03:00 +0300

    feature: implement caching to reserve service

--- src/Infrastructure/Persistence/Services/ReserveService.cs
+++ src/Infrastructure/Persistence/Services/ReserveService.cs
@@ -2,6 +2,7 @@
 using Application.Persistence.Repositories;
 using Application.Persistence.Services;
 using AutoMapper;
+using CacheServices.Service;
 using Domain.Entities;
 
 namespace Infrastructure.Persistence.Services;
@@ -10,17 +11,28 @@ public class ReserveService:IReserveService
 {
     private readonly IReserveRepository _reserveRepository;
     private readonly IMapper _mapper;
+    private readonly ICacheService _cacheService;
 
-    public ReserveService(IReserveRepository reserveRepository,IMapper mapper)
+    public ReserveService(IReserveRepository reserveRepository,IMapper mapper,ICacheService cacheService)
     {
         _reserveRepository = reserveRepository;
         _mapper = mapper;
+        _cacheService = cacheService;
     }
     public IEnumerable<ReserveDto> GetAllReserves(int userId)
     {
+        var cacheData = _cacheService.GetData<IEnumerable<ReserveDto>>("reserve");
+        if (cacheData != null && cacheData.Count() > 0)
+        {
+            return cacheData;
+        }
         var reserves = _reserveRepository.GetAll(obj => obj.UserId == userId);
-        var reservesDto = _mapper.Map<IEnumerable<ReserveDto>>(reserves);
-        return reservesDto;
+        cacheData = _mapper.Map<IEnumerable<ReserveDto>>(reserves);
+        
+        var expirationTime = DateTime.Now.AddMinutes(3);
+        _cacheService.SetData("reserve",cacheData,expirationTime);
+        
+        return cacheData;
     }
 
     public async Task<IEnumerable<ReserveDto>> GetReservesByDateAsync(DateTime date)
@@ -35,6 +47,7 @@ public class ReserveService:IReserveService
     {
         _reserveRepository.Delete(reserve);
         await _reserveRepository.SaveChangesAsync();
+        _cacheService.RemoveData($"reserve{reserve.Id}");
     }
 
     public async Task CreateReserveAsync(UpsertReserveDto reserveDto)
@@ -42,5 +55,8 @@ public class ReserveService:IReserveService
         var reserve = _mapper.Map<Reserve>(reserveDto);
         await _reserveRepository.InsertAsync(reserve);
         await _reserveRepository.SaveChangesAsync();
+        var dto = _mapper.Map<ReserveDto>(reserve);
+        var expirationTime = DateTime.Now.AddMinutes(3);
+        _cacheService.SetData($"reserve{reserve.Id}",dto,expirationTime);
     }
 }
